cd14c3f2f619994989b3be04ccc4b17d7f38d33f
dongjin lee <ldj0412@kstec.co.kr>
2020-11-25 12:14:41 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+# 파일정리에서 제외될 확장자
+exceptList = ['json', 'ini', 'exe']
+
+# 작업할 폴더 경로
+workDir = '.\\Takeout\\Google 포토'
+
+# func 전체 대상 파일 계산
+
+# func 폴더 탐색
+
+# 폴더 생성(월단위)
+
+# 파일명 변경
+
+# 파일 생성일, 수정일 변경
+
+
+if __name__ == '__main__':
+    print('hi')
